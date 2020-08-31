7da8bee2c504d7563a754bfed2a5a5ca173e4cde
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2020-08-31 23:44:49 +0500

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
+const os = require('os');
 
 module.exports.run = async (client, message, args) => {
     if (message.author.bot) return;
@@ -9,14 +10,24 @@ module.exports.run = async (client, message, args) => {
     let servercount = client.guilds.cache.size;
     let usercount = client.users.cache.size;
     let channelscount = client.channels.cache.size;
+    let arch = os.arch();
+    let platform = os.platform();
+    let shard = client.ws.shards.size;
+    let NodeVersion = process.version;
+    let cores = os.cpus().length;
 
     let stats = new Discord.MessageEmbed()
+    .setAuthor('DevMirza')
     .setTitle(`Statistics of ${client.user.username}`)
     .setColor('BLUE')
-    .addField("Server Count", `${servercount}`)
-    .addField("Users Count", `${usercount}`)
-    .addField("Channel's Count", `${channelscount}`)
-    .addField("Hosted Platform", "Hosting on Glitch with 99% uptime")
+    .addField("Server Count", `${servercount}`, true)
+    .addField("Users Count", `${usercount}`, true)
+    .addField("Channel's Count", `${channelscount}`, true)
+    .addField('Architecture', `${arch}`, true)
+    .addField('Platform', `${platform}`, true)
+    .addField('Node-Version', `${NodeVersion}`, true)
+    .addField('Shards', `${shard}`, true)
+    .addField('Cores', `${cores}`, true)
     .setTimestamp()
     .setFooter(`${message.author.tag}`, message.author.displayAvatarURL());
     message.channel.send(stats);
