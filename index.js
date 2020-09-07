ae4392e66f1ec868995820146f9688d3ae087603
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2020-09-07 13:30:22 +0500

    Remove exemptPermissions

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ client.giveawaysManager = new GiveawaysManager(client, {
     updateCountdownEvery: 3000,
     default: {
         botsCanWin: false,
-        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
         embedColor: "#FF0000",
         reaction: "🎉"
     }
