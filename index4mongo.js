d1abc17452b80ee315aeb5aa2ddd3509f7d35e92
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-03-04 01:38:36 +0500

    New Updates, cleanup the code, added two example files for custom database.

---
+++ index4mongo.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('./config.json');
+client.config = config;
+
+// Load quickmongo.
+const { Database } = require('quickmongo');
+const db = new Database(`${config.mongodb_url}`);
+db.once('ready', async () => {
+    if ((await db.get('giveaways')) === null) await db.set('giveaways', []);
+    console.log('SUCCESS!');
+});
+
+// Init discord giveaways
+const { GiveawaysManager } = require('discord-giveaways');
+class GiveawayManagerWithOwnDatabase extends GiveawaysManager {
+    // This function is called when the manager needs to get all the giveaway stored in the database.
+    async getAllGiveaways() {
+        // Get all the giveaway in the database
+        return await db.get('giveaways');
+    }
+
+    // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
+    async saveGiveaway(messageID, giveawayData) {
+        // Add the new one
+        await db.push('giveaways', giveawayData);
+        // Don't forget to return something!
+        return true;
+    }
+
+    async editGiveaway(messageID, giveawayData) {
+        // Gets all the current giveaways
+        const giveaways = await db.get('giveaways');
+        // Remove the old giveaway from the current giveaways ID
+        const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID);
+        // Push the new giveaway to the array
+        newGiveawaysArray.push(giveawayData);
+        // Save the updated array
+        await db.set('giveaways', newGiveawaysArray);
+        // Don't forget to return something!
+        return true;
+    }
+
+    async deleteGiveaway(messageID) {
+        // Gets all the current giveaways
+        const data = await db.get('giveaways');
+        // Remove the giveaway from the array
+        const newGiveawaysArray = data.filter((giveaway) => giveaway.messageID !== messageID);
+        // Save the updated array
+        await db.set('giveaways', newGiveawaysArray);
+        // Don't forget to return something!
+        return true;
+    }
+}
+
+const manager = new GiveawayManagerWithOwnDatabase(client, {
+    storage: false,
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+client.giveawaysManager = manager;
+
+/* Load all events */
+fs.readdir("./events/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`👌 Event loaded: ${eventName}`);
+        client.on(eventName, event.bind(null, client));
+        delete require.cache[require.resolve(`./events/${file}`)];
+    });
+});
+
+client.commands = new Discord.Collection();
+
+/* Load all commands */
+fs.readdir("./commands/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+        console.log(`👌 Command loaded: ${commandName}`);
+    });
+});
+
+// Login
+client.login(config.token);
