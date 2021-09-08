c33b53f64271479494e9e7ec50b7a2cd092ae3c5
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-09-08 23:05:04 +0500

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,5 @@
 module.exports = (client, interaction) => {
+
     if (!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
@@ -7,6 +8,6 @@ module.exports = (client, interaction) => {
         content: `Command \`${interaction.commandName}\` not found.`,
         ephemeral: true
     });
-
+  
     command.run(client, interaction);
 };
