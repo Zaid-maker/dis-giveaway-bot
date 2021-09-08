4c373f645ff5dd467a45100779b218578687380a
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-09-08 23:04:38 +0500

    Rename

---
+++ events/interactionCreate.js
@@ -0,0 +1,12 @@
+module.exports = (client, interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return void interaction.reply({
+        content: `Command \`${interaction.commandName}\` not found.`,
+        ephemeral: true
+    });
+
+    command.run(client, interaction);
+};
