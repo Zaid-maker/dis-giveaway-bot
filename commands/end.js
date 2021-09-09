359a1e5b8ee93dca95cbbdc4f4749c73b6960e8e
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-09-09 20:06:19 +0500

    Update end.js

--- commands/end.js
+++ commands/end.js
@@ -1,45 +1,58 @@
 const ms = require('ms');
+const messages = require('../utils/messages');
 
-exports.run = async (client, message, args) => {
-
-    // If the member doesn't have enough permissions
-    if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
-        return message.channel.send(':x: You need to have the manage messages permissions to reroll giveaways.');
-    }
-
-    // If no message ID or giveaway name is specified
-    if(!args[0]){
-        return message.channel.send(':x: You have to specify a valid message ID!');
-    }
-
-    // try to found the giveaway with prize then with ID
-    let giveaway =
-    // Search with giveaway prize
-    client.giveawaysManager.giveaways.find((g) => g.prize === args.join(' ') && g.guildID === message.guild.id) ||
-    // Search with giveaway ID
-    client.giveawaysManager.giveaways.find((g) => g.messageID === args[0] && g.guildID === message.guild.id);
-
-    // If no giveaway was found
-    if(!giveaway){
-        return message.channel.send('Unable to find a giveaway for `'+ args.join(' ') + '`.');
-    }
-
-    // Edit the giveaway
-    client.giveawaysManager.edit(giveaway.messageID, {
-        setEndTimestamp: Date.now()
-    })
-    // Success message
-    .then(() => {
-        // Success message
-        message.channel.send('Giveaway will end in less than '+(client.giveawaysManager.options.updateCountdownEvery/1000)+' seconds...');
-    })
-    .catch((e) => {
-        if(e.startsWith(`Giveaway with message ID ${giveaway.messageID} is already ended.`)){
-            message.channel.send('This giveaway is already ended!');
-        } else {
-            console.error(e);
-            message.channel.send('An error occured...');
+module.exports =  {
+    description: 'End a Giveaway',
+    options: [
+        {
+            name: 'giveaway',
+            description: 'The giveaway to end (message ID or Giveaway prize.',
+            type: 'STRING',
+            required: true
         }
-    });
+    ],
+
+    run: async (client, interaction) => {
+        // If the member doesn't have enough permissions
+        if(!interaction.member.permission.has('MANAGE_MESSAGES') && !interaction.member.roles.cache.some((r) => r.name === "Giveaways")){
+            return interaction.reply({
+                content: ':x: You need to have the manage messages permissions to end giveaways.',
+                ephemeral: true
+            });
+        }
+
+
+        const query = interaction.options.getString('giveaway');
+
+        // try to found the giveaway with prize then with ID
+        const giveaway =
+        // Search with giveaway prize
+        client.giveawaysManager.giveaways.find((g) => g.prize === query && g.guildId === message.guild.id) ||
+        // Search with giveaway ID
+        client.giveawaysManager.giveaways.find((g) => g.messageID === query && g.guildId === message.guild.id);
+
+        // If no giveaway was found
+        if(!giveaway){
+            return interaction.reply({
+                content: 'Unable to find a giveaway for `'+ query + '`.',
+                ephemeral: true
+            });
+        }
+
+        // Edit the giveaway
+        client.giveawaysManager.end(giveaway.messageId)
+        // Success message
+        .then(() => {
+            // Success message
+            interaction.reply('Giveaway Ended!')
+        })
+        .catch((e) => {
+            interaction.reply({
+                content: e,
+                ephemeral: true
+            })
+        });
+
+    }
 
 };
