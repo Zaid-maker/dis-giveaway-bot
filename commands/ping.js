eb9554a476d3be8bcd57ae5dcd28723b9cb72c02
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2021-09-10 14:47:08 +0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,28 +1,31 @@
-const Discord = require('discord.js');
-const config = require('../config.json');
+const Discord = require("discord.js");
+const config = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
-  if(message.author.bot) return;
+  if (message.author.bot) return;
 
   let prefix = config.prefix;
 
-  if(!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(prefix)) return;
 
-  const m = await message.channel.send("Hold on......")
+  const m = await message.channel.send("Hold on......");
 
   let pong = new Discord.MessageEmbed()
-  .setTitle("🏓 Pong!")
-  .setColor("DARK BLUE")
-  .setTimestamp()
-  .addFields(
-    {name: 'Message Response Time', value: `${m.createdTimestamp - message.createdTimestamp}ms`, inline: true},
-    {name: 'Client Ping', value: `${Math.round(client.ws.ping)}ms`}
-  )
-  .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
+    .setTitle("🏓 Pong!")
+    .setColor("DARK BLUE")
+    .setTimestamp()
+    .addFields(
+      {
+        name: "Message Response Time",
+        value: `${m.createdTimestamp - message.createdTimestamp}ms`,
+        inline: true,
+      },
+      { name: "Client Ping", value: `${Math.round(client.ws.ping)}ms` }
+    )
+    .setFooter(
+      `Requested by ${message.author.tag}`,
+      message.author.displayAvatarURL()
+    );
 
-  m.edit(pong)
-}
-
-module.exports.help = {
-  name: "ping"
-}
+  m.edit(pong);
+};
