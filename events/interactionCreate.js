25f2bc1961734ec5740996f4a94ee1fb53725608
Zaid-maker <pzhafeez@gmail.com>
2022-11-20 19:56:55 +0500

    update event

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,5 @@
 module.exports = (client, interaction) => {
-  if (!interaction.isCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
 
