914602f0f2617c9e206aebad599ad7d9859f1f7b
Zaid-maker <pzhafeez@gmail.com>
2022-12-02 22:18:48 +0500

    fix help command

--- commands/help.js
+++ commands/help.js
@@ -1,31 +1,38 @@
 const messages = require("../utils/messages");
-const { EmbedBuilder, MessageActionRow, MessageButton } = require("discord.js");
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 
 module.exports = {
   name: "help",
   description: "Get all Bot Commands",
   run: async (client, interaction) => {
     let helpembed = new EmbedBuilder();
-    helpembed.setColor("RANDOM");
     helpembed.setAuthor({ name: `Commands of ${client.user.username}` });
     helpembed.setColor("#2f3136");
     helpembed.setThumbnail(
       "https://cdn.discordapp.com/attachments/833753190859407411/910562424258715738/DisGiveaway-01-01.png"
     );
     client.commands.map((cmd) => {
-      helpembed.addField(`\`${cmd.name}\``, `${cmd.description}`, true);
+      helpembed.addFields({
+        name: `\`${cmd.name}\``,
+        value: `${cmd.description}`,
+      });
     });
     helpembed.setTimestamp();
     helpembed.setFooter({ text: `© DisGiveaway | Have a nice day!` });
 
-    const row = new MessageActionRow().addComponents(
-      new MessageButton()
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
         .setEmoji("865572290065072128")
         .setLabel(`Invite ${client.user.username}`)
         .setURL(
           `https://discord.com/api/oauth2/authorize?client_id=910559370843131944&permissions=8&scope=bot%20applications.commands`
         )
-        .setStyle("LINK")
+        .setStyle(ButtonStyle.Link)
     );
 
     await interaction.reply({
